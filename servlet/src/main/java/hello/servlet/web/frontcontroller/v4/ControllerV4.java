00b25460abecfe277c6bbd553b4763d93e5d76b3
haejung98 <haejung98@naver.com>
2022-02-12 02:38:52 +0900

    servlet

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,13 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import java.util.Map;
+
+public interface ControllerV4 {
+
+    /**
+     * @param paramMap
+     * @param model
+     * @return viewName
+     */
+    String process(Map<String, String> paramMap, Map<String, Object> model); // model 객체 파라미터로 전달, 뷰 이름만 반환
+}
